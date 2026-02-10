c90a7c5d54d14cc79dc67264b3dc48940343b929
Sirigireddy11 <harshasirigireddy11@gmail.com>
2026-02-10 21:26:22 +0530

    Create commentType.C

---
+++ commentType.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char line[200];
+    printf("Enter a line: ");
+    fgets(line, sizeof(line), stdin);
+    line[strcspn(line, "\n")] = '\0';
+    if (strncmp(line, "//", 2) == 0)
+        printf("Single-line comment\n");
+    else if (strncmp(line, "/*", 2) == 0 && strstr(line, "*/") != NULL)
+        printf("Multi-line comment\n");
+    else
+        printf("Not a comment\n");
+    return 0;
+}
